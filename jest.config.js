11c9254cd63f536c8ee566b3eeae93bc64fa2d54
SirFizX <eeisaman@auburnschl.edu>
2025-03-23 17:58:07 -0400

    fix: Consolidate Jest configuration into jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,18 @@ export default {
     }],
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
-}; 
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
