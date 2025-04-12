69d68babb483f06107a3350ee679780db42d21fc
FrameBuffer4096 <eeisaman@virbela.com>
2025-04-12 16:38:32 -0400

    update configs for jest and ts

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,17 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^srcRoot/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
         useESM: true,
-        tsconfig: 'tsconfig.json',
+        tsconfig: {
+          module: 'ES2022',
+          moduleResolution: 'node',
+        },
       },
     ],
   },
@@ -24,14 +28,4 @@ export default {
   transformIgnorePatterns: ['node_modules/(?!(@babylonjs)/)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-      tsconfig: {
-        module: 'ESNext',
-        target: 'ES2020',
-        moduleResolution: 'node',
-      },
-    },
-  },
 };
